9e01575aa9cc4bcfea4a7c49ca344cf890dd8985
Lucas Lindh <lucas.lindh6@live.se>
2026-01-22 15:30:56 +0100

    cleaning

--- src/taxipred/frontend/pages/home.py
+++ src/taxipred/frontend/pages/home.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from pathlib import Path
+from taxipred.utils.constants import TAXI_PATH
 
-taxi_path = Path(__file__).parents[2] / "data" / "taxi.jpg"
 
 st.markdown(
     "<p style='text-align: center; font-size: 50px; font-weight: 600;'>Taxipred App</p>",
     unsafe_allow_html=True
 )
 st.markdown("### This is a streamlit app that consumes a FastAPI about taxi prices. With the app you can predict the cost of taxi rides. Put in the information about your trip and you will get a price prediction.")
-st.image(taxi_path)
+st.image(TAXI_PATH)
